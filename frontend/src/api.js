cdada0abd44c140fa6a3e196cb97b9bd38007f4a
Ashi Dhandia <ashidhandia6@gmail.com>
2026-01-05 12:34:32 +0800

    Modified analyzeProfile to add forceRefresh param.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// This points to your Python backend
+// points to the python backend
 const API_BASE_URL = "http://127.0.0.1:8000";
 
 export const api = axios.create({
@@ -10,8 +10,8 @@ export const api = axios.create({
   },
 });
 
-// The function to trigger the analysis
-export const analyzeProfile = async (url) => {
-  const response = await api.post("/analyze", { url, is_cs_ai: true });
+// function to trigger analysis
+export const analyzeProfile = async (url, forceRefresh = false) => {
+  const response = await api.post("/analyze", { url, is_cs_ai: true, forceRefresh: forceRefresh });
   return response.data;
 };
